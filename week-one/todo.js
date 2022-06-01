2638d4986dc2b4ec8c64311408cb3cad4d1ac370
hoon2hooni <sanghoon.engineer@gmail.com>
2022-06-01 10:47:46 +0900

    1주차 과제 한상훈 문제 풀이

--- week-one/todo.js
+++ week-one/todo.js
@@ -1,11 +1,14 @@
-
 //1번
 export const calculator = (presentDict) => {
-	/**todo */
-}
-
+  /**todo */
+  const prices = Object.values(presentDict);
+  return prices.reduce((prev, curr) => prev + curr);
+};
 
 export const getSumDivisible = (divisor, numberList) => {
-	/**todo */
-	
-}
+  /**todo */
+  const result = numberList
+    .filter((num) => num % divisor === 0)
+    .reduce((prev, curr) => prev + curr);
+  return result;
+};
