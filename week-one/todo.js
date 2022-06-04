8ceb9c3dbef9b06af16201f775655b18ba27579c
youngeun choi <choi46910@naver.com>
2022-06-04 09:47:14 +0900

    1주차 과제 최영은 문제 풀이

--- week-one/todo.js
+++ week-one/todo.js
@@ -2,10 +2,28 @@
 //1번
 export const calculator = (presentDict) => {
 	/**todo */
+
+	let answer = 0;
+	let price = Object.values(presentDict);
+	for (let i = 0; i < price.length; i++) {
+		answer += price[i];
+	}
+	return (answer);
 }
 
-
+const divisor = 3;
+const numberList = [3, 2, 4, 7, 9, 10, 12, 40];
 export const getSumDivisible = (divisor, numberList) => {
 	/**todo */
-	
+	let answer = 0;
+	let list = [];
+	for (let i = 0; i < numberList.length; i++) {
+		if (numberList[i] % divisor == 0) {
+			list.push(numberList[i]);
+		}
+	}
+	for (let i = 0; i < list.length; i++) {
+		answer += list[i];
+	}
+	return answer;
 }
